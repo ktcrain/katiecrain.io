de0a1f0c720e31ab5d28a04ffdf2774ed1c9c480
Katie Crain <kt.crain@gmail.com>
2021-02-17 15:05:26 -0500

    fix server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, "../client", "build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
